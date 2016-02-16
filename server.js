0d8da575d6c5985dc04193eed6da6c8ed9135db1
mdeumie <2m1tema@gmail.com>
2016-02-16 14:19:36 +0100

    test callback dans wkhtmltopdf

--- server.js
+++ server.js
@@ -18,24 +18,29 @@ app.post('/buildPDF', function(req,res){
     console.log(req.body.htmlToRender);
 
     ///TODO : handle promises
-    /*fs.unlink('./generated_files/result.pdf', function(err) {
+    fs.unlink('./generated_files/result.pdf', function(err) {
         if(err) {
             return console.log(err);
         }
         console.log("The pdf file was deleted!");
 
-        wkhtmltopdf(req.body.htmlToRender).pipe(fs.createWriteStream('./generated_files/result.pdf'));
-
-    });*/
-
-    fs.readFile('./generated_files/ticket.pdf', function(err,data){
-        if(err){
-            res.json({'status':'error', msg:err});
-        }else{
-            res.writeHead(200, {"Content-Type": "application/pdf"});
-            res.write(data);
-            res.end();
-        }
+        wkhtmltopdf(req.body.htmlToRender, function(code, signal) {
+            console.log(code);
+            console.log(signal);
+            fs.createWriteStream('./generated_files/result.pdf', function(err) {
+                if(err) console.log(err);
+                
+                fs.readFile('./generated_files/result.pdf', function(err,data){
+                    if(err){
+                        res.json({'status':'error', msg:err});
+                    }else{
+                        res.writeHead(200, {"Content-Type": "application/pdf"});
+                        res.write(data);
+                        res.end();
+                    }
+                });
+            });
+        });
     });
 
 });
